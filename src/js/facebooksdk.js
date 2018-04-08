3943f5c97cd2411702ccdf8cb9c28318ac89b84b
BrOrlandi <brorlandi@gmail.com>
2018-04-08 00:06:02 -0300

    Update facebook like button.

--- src/js/facebooksdk.js
+++ src/js/facebooksdk.js
@@ -1,20 +1,9 @@
 if ('production' === process.env.NODE_ENV) {
-
-window.fbAsyncInit = function() {
-  FB.init({
-    appId      : '966673750071792',
-    xfbml      : true,
-    version    : 'v2.5'
-  });
-};
-
-(function(d, s, id){
-   var js, fjs = d.getElementsByTagName(s)[0];
-   if (d.getElementById(id)) {return;}
-   js = d.createElement(s); js.id = id;
-   js.src = "//connect.facebook.net/en_US/sdk.js";
-   fjs.parentNode.insertBefore(js, fjs);
- }(document, 'script', 'facebook-jssdk'));
-
-
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&autoLogAppEvents=1&version=v2.12&appId=966673750071792';
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
 }
