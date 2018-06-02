ec3a391839d33336518cafa0c9e1edf401b32dd9
BrOrlandi <brorlandi@gmail.com>
2018-06-02 13:09:46 -0300

    Fix retry request now includes query params.

--- src/js/api/Http.js
+++ src/js/api/Http.js
@@ -17,6 +17,7 @@ const _retryLastRequest = ({ config }, resolve) => {
     method: config.method,
     url: config.url,
     data: config.data ? JSON.parse(config.data) : null,
+    params: config.params,
   };
 
   return Http().request(options)
