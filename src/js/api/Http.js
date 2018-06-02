4fdd1722662ac65b7d7e5d964d3d58f72ce8b111
BrOrlandi <brorlandi@gmail.com>
2018-06-02 12:48:09 -0300

    Improve error on request for Sentry.

--- src/js/api/Http.js
+++ src/js/api/Http.js
@@ -27,7 +27,7 @@ const _retryLastRequest = ({ config }, resolve) => {
 };
 
 const _errorInterceptor = (error) => {
-  const errorData = error.response;
+  // const errorData = error.response;
   // if (!errorData) {
   //   Raven.captureBreadcrumb({
   //     message: `Unknow error on request, probably timeout error. Error code: ${error.code}`,
@@ -42,10 +42,10 @@ const _errorInterceptor = (error) => {
       Tries.reset();
       reject(error);
       Raven.captureBreadcrumb({
-        message: `Error on request: ${error.code}`,
+        message: `Error on request. Error code: ${error.code}`,
         level: 'error',
       });
-      _sendRavenNotification(errorData);
+      _sendRavenNotification(JSON.stringify(error));
       return;
     }
 
