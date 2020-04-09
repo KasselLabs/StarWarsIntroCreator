177b61008adbba7165ca1658f0ed758155388458
Nihey Takizawa <nihey.takizawa@gmail.com>
2020-04-08 22:33:25 -0300

    Brian's intro customization

--- src/js/StarWarsAnimation.js
+++ src/js/StarWarsAnimation.js
@@ -29,8 +29,27 @@ class StarWarsAnimation {
 
     const paragraphs = escapedText
       .trim()
+      .replace(/(\s\s[\s]+|\t+)/g, '|')
       .split('\n')
+      .map((p) => {
+        const split = p.split('|').slice(0, 2);
+        console.log(split);
+        if (1 === split.length) {
+          return p;
+        }
+
+        return `<div class="credit-section"><div>${split.join('</div><div>')}</div></div>`;
+      })
+      .map((p) => {
+        const split = p.split('++');
+        if (1 === split.length) {
+          return p;
+        }
+
+        return `<div class="credit-header"><div>${split.join('</div><div>')}</div></div>`;
+      })
       .join('</p><p>');
+    console.log(paragraphs);
 
     const breakLineBetweenPs = paragraphs
       .split('<p></p>')
@@ -69,49 +88,12 @@ class StarWarsAnimation {
 
     // TEXT CENTER ALIGNMENT
     textContainer.style.textAlign = opening.center ? 'center' : ''; // empty will not override the justify default rule
-
-    // LOGO
-    const starwarsDefaultText = 'star\nwars';
-    const logoTextContainer = animation.querySelector('.logoText');
-    const logoDefaultContainer = animation.querySelector('#logoDefault');
-
-    const logoText = opening.logo;
-    // is default logo
-    if (logoText.toLowerCase() === starwarsDefaultText) {
-      logoTextContainer.style.display = 'none';
-      logoDefaultContainer.style.display = 'block';
-
-      return;
-    }
-
-    const logoTextSplit = logoText.split('\n');
-    const word1 = logoTextSplit[0];
-    const word2 = logoTextSplit[1] || '';
-
-    const wordContainers = logoTextContainer.querySelectorAll('div');
-    wordContainers[0].textContent = word1;
-    wordContainers[1].textContent = word2;
-
-    logoTextContainer.style.display = 'flex';
-    logoDefaultContainer.style.display = 'none';
   }
 
   play() {
-    const DEFAULT_LENGTH = 1977;
-    const ANIMATION_CONSTANT = 0.04041570438799076;
-    const FINAL_POSITION = 20;
-
     this.animationContainer.appendChild(this.animation);
 
     const textContainer = this.animation.querySelector('#text');
-
-    // adjust animation speed
-    const titlesContainer = this.animation.querySelector('#titles > div');
-    if (titlesContainer.offsetHeight > DEFAULT_LENGTH) {
-      const exceedSize = titlesContainer.offsetHeight - DEFAULT_LENGTH;
-      const animationFinalPosition = FINAL_POSITION - (exceedSize * ANIMATION_CONSTANT);
-      appendKeyframesRule('titlesAnimation', `100% { top: ${animationFinalPosition}% }`);
-    }
     setTimeout(() => {
       checkChromeBugOnContainer(textContainer, true);
     }, 1500);
