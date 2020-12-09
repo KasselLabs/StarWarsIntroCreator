9c1c6d9a9649d3cd1b404fa5dc8e41d4d0b50e07
BrOrlandi <brorlandi@gmail.com>
2020-12-09 18:23:17 -0300

    Remove iOS download advices.

--- src/js/DownloadPage/RenderedPage.js
+++ src/js/DownloadPage/RenderedPage.js
@@ -1,13 +1,8 @@
 import React from 'react';
-import bowser from 'bowser';
 
 import ContactButton from './ContactButton';
 import DownloadVideoButton from './DownloadVideoButton';
 import Atat from './Atat';
-
-let iOSDevice = 'iOS device';
-iOSDevice = bowser.iphone ? 'iPhone' : iOSDevice;
-iOSDevice = bowser.ipad ? 'iPad' : iOSDevice;
 
 const RenderedPage = ({ status }) => (
   <div>
@@ -15,13 +10,6 @@ const RenderedPage = ({ status }) => (
     <p>
       Your video is ready to download! Click on the button below to download it!
     </p>
-    {bowser.ios &&
-      <p>
-        To download your video on your {iOSDevice} open this page using the app&nbsp;
-        <a href="https://itunes.apple.com/us/app/documents-by-readdle/id364901807?mt=8">Documents by Readdle</a>.
-        And follow this <a href="https://youtu.be/XZshsVMjsp8">video tutorial</a> on how to download.
-      </p>
-    }
     <DownloadVideoButton url={status.downloadUrl} />
     <ContactButton customText="If you have any problem while trying to download the video, please contact us:" />
   </div>
