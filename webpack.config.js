218e1e29fe44f0f0302d45b70b5b37b5898c463b
Nihey Takizawa <nihey.takizawa@gmail.com>
2021-04-16 12:16:15 -0300

    Add dotenv-webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
-const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   devServer: {
@@ -40,17 +40,7 @@ module.exports = {
     filename: '[hash].js',
   },
   plugins: [
-    new webpack.EnvironmentPlugin([
-      'RAVEN',
-      'FIREBASE_INITIAL',
-      'FIREBASE_A',
-      'FIREBASE_B',
-      'FIREBASE_C',
-      'FIREBASE_D',
-      'SERVER_API',
-      'PAYMENT_PAGE_URL',
-      'FACEBOOK_PIXEL',
-    ]),
+    new Dotenv(),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'src', 'index.html'),
     }),
