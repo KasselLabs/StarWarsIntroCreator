ac41ef3b16a0d909149e53c2642ad75a35fe9df4
Nihey Takizawa <nihey.takizawa@gmail.com>
2022-09-30 10:48:18 -0300

    refactor(donate): Remove Donate Button

--- src/js/ViewController.js
+++ src/js/ViewController.js
@@ -58,23 +58,6 @@ class ViewController {
         e.preventDefault();
         UrlHandler.goToEditPage(ApplicationState.state.key);
       });
-
-    document.querySelector('#donateFooterButton').addEventListener('click', (e) => {
-      e.preventDefault();
-      const openingKey = ApplicationState.state.key;
-      const isDefaultKey = openingKey === defaultKey;
-      if (isDefaultKey) {
-        swal({
-          title: 'donate',
-          html: `<p>You can donate to support us and receive your creation rendered as a video.</p>
-          <p>But first, you need to create your own content, edit the fields, and click on the PLAY button to make it.</p>
-          <p>Then you can request your Download or click on this button to donate for it.</p>`,
-        });
-        return;
-      }
-
-      UrlHandler.goToDownloadPage(openingKey);
-    });
   }
 
   setLoading() {
